f2bcb2c7e8aa334ac248845e9b6a383deba6e986
Will Boyd <shazzenbot@gmail.com>
2019-08-07 11:24:38 -0400

    add end state for stateless transformations example

--- src/main/java/com/linuxacademy/ccdak/streams/StatelessTransformationsMain.java
+++ src/main/java/com/linuxacademy/ccdak/streams/StatelessTransformationsMain.java
@@ -1,12 +1,16 @@
 package com.linuxacademy.ccdak.streams;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Properties;
 import java.util.concurrent.CountDownLatch;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.kstream.KStream;
 
 /*import java.util.Properties;
 import java.util.concurrent.CountDownLatch;*/
@@ -16,7 +20,7 @@ public class StatelessTransformationsMain {
     public static void main(String[] args) {
         // Set up the configuration.
         final Properties props = new Properties();
-        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "inventory-data");
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "stateless-transformations-example");
         props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         props.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, 0);
         // Since the input topic uses Strings for both key and value, set the default Serdes to String.
@@ -25,8 +29,36 @@ public class StatelessTransformationsMain {
 
         // Get the source stream.
         final StreamsBuilder builder = new StreamsBuilder();
+        final KStream<String, String> source = builder.stream("stateless-transformations-input-topic");
         
-        //Implement streams logic.
+        // Split the stream into two streams, one containing all records where the key begins with "a", and the other containing all other records.
+        KStream<String, String>[] branches = source
+            .branch((key, value) -> key.startsWith("a"), (key, value) -> true);
+        KStream<String, String> aKeysStream = branches[0];
+        KStream<String, String> othersStream = branches[1];
+        
+        // Remove any records from the "a" stream where the value does not also start with "a".
+        aKeysStream.filter((key, value) -> value.startsWith("a"));
+        
+        // For the "a" stream, convert each record into two records, one with an uppercased value and one with a lowercased value.
+        aKeysStream.flatMap((key, value) -> {
+            List<KeyValue<String, String>> result = new LinkedList<>();
+            result.add(KeyValue.pair(key, value.toUpperCase()));
+            result.add(KeyValue.pair(key, value.toLowerCase()));
+            return result;
+        });
+        
+        // For the "a" stream, modify all records by uppercasing the key.
+        aKeysStream.map((key, value) -> KeyValue.pair(key.toUpperCase(), value));
+        
+        //Merge the two streams back together.
+        KStream<String, String> mergedStream = aKeysStream.merge(othersStream);
+        
+        //Print each record to the console.
+        mergedStream.peek((key, value) -> System.out.println("key=" + key + ", value=" + value));
+        
+        //Output the transformed data to a topic.
+        mergedStream.to("stateless-transformations-output-topic");
         
         final Topology topology = builder.build();
         final KafkaStreams streams = new KafkaStreams(topology, props);
